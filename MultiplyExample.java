1dc60a963e19527446d4104ac0bc9d1e96935a8c
aditi5704 <w24.aditi636.northern@purescollege.ca>
2024-09-15 18:17:26 -0400

    Add files via upload

---
+++ MultiplyExample.java
@@ -0,0 +1,13 @@
+public class MultiplyExample {
+    public static void main(String[] args) {
+        // Declare two variables to hold the numbers
+        int num1 = 4;
+        int num2 = 5;
+
+        // Calculate the product
+        int product = num1 * num2;
+
+        // Print the result
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + product);
+    }
+}
